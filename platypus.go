72c10fc8c089f7424e9ed3c5f9577b4939adf320
whytheplatypus <whytheplatypus@gmail.com>
2018-12-30 11:52:48 -0500

    Write to Discard

--- platypus.go
+++ platypus.go
@@ -3,6 +3,7 @@ package platypus
 import (
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"os"
 )
 
@@ -10,7 +11,7 @@ var platypus = []byte{0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20
 
 func init() {
 	pf := flag.NewFlagSet("platypus", flag.ContinueOnError)
-	pf.SetOutput(os.DevNull)
+	pf.SetOutput(ioutil.Discard)
 	var p bool
 	pf.BoolVar(&p, "platypus", false, "Why the platypus?")
 	pf.Parse(os.Args[1:])
